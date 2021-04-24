60be91f0629e545fe3c8f78f99153405822069aa
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-24 20:36:30 -0300

    refactor: put name saved in the storage in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { View, Text, StyleSheet, Image } from "react-native";
 import colors from "../styles/colors";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import AvatarImg from "../assets/gabriel.jpeg";
 import fonts from "../styles/fonts";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function Header() {
+  const [userName, setUserName] = useState<string>();
+
+  /**TODO - Create a usePersisentState hook and context  to save user info and get in the app */
+
+  useEffect(() => {
+    async function getUserFromStorage() {
+      const user = await AsyncStorage.getItem("@plantmanager:user");
+      if (user) {
+        setUserName(user);
+      }
+    }
+    getUserFromStorage();
+  }, []);
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.greetings}>Olá</Text>
-        <Text style={styles.userName}>Gabriel</Text>
+        <Text style={styles.greetings}>Olá,</Text>
+        <Text style={styles.userName}>{userName}</Text>
       </View>
       <Image source={AvatarImg} width={200} height={200} style={styles.image} />
     </View>

e8929d7cd42cf012417657d51c273022505eeabc
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-24 00:44:46 -0300

    feat: install expo icons lib

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,10 +6,11 @@ import {
   TouchableOpacity,
   StyleSheet,
   SafeAreaView,
+  Dimensions,
 } from "react-native";
 import wateringImg from "../assets/watering.png";
 import colors from "../styles/colors";
-import { Button } from "../components/Button";
+import { Entypo } from "@expo/vector-icons";
 
 export function Welcome() {
   return (
@@ -20,14 +21,18 @@ export function Welcome() {
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar sempre
         que você precisar
       </Text>
 
-      <Button title=">" onPress={() => console.log("vkdbfnk")} />
+      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>
+          <Entypo name="chevron-right" style={styles.buttonIcon} />
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -35,7 +40,7 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     alignItems: "center",
   },
   title: {
@@ -51,9 +56,23 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading,
   },
-
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
+  },
+  buttonIcon: {
+    fontSize: 28,
+    color: colors.white,
   },
 });

fcd6ba1ab3387470d08441d1bcbc6984025dabcc
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-24 00:49:19 -0300

    feat: add custom font Jost from google

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Text, View, StatusBar } from "react-native";
 import { Welcome } from "./src/pages/Welcome";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <View />;
+  }
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />

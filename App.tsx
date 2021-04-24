d19ad6420aea7653ed19531d3d4bc07a3d7b1c28
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-24 00:59:17 -0300

    feat: Finish welcome page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View, StatusBar } from "react-native";
 import { Welcome } from "./src/pages/Welcome";
+import AppLaoding from "expo-app-loading";
 import {
   useFonts,
   Jost_400Regular,
@@ -14,7 +15,7 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
-    return <View />;
+    return <AppLaoding />; // keep our splash page while fonts not loaded
   }
   return (
     <>
